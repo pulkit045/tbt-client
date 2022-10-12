cd08ffcd12c73dc420769a00cd17da69b5c5f773
pulkit045 <pulkitgupta045@gmail.com>
2022-10-12 17:02:31 +0530

    added home

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Markup from './components/Markup';
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      {/* <span>navbar</span> */}
+      <Markup />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+const Header = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/">
+          <h3 className="logo">
+            {/* <img src={Logo} alt="" title="logo" /> */}
+            Logo
+          </h3>
+        </Link>
+        <ul
+          className={isMobile ? "nav-links-mobile" : "nav-links"}
+          onClick={() => setIsMobile(false)}
+        >
+          <Link to="/login" className="login">
+            <li>Login</li>
+          </Link>
+          <Link to="/register" className="signup">
+            <li>Sign up</li>
+          </Link>
+        </ul>
+        <button
+          className="mobile-menu-icon"
+          onClick={() => setIsMobile(!isMobile)}
+        >
+          {isMobile ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-bars"></i>
+          )}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
